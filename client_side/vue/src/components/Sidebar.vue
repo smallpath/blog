23697adf8215ff92b263afad2096ca3d55be0b9b
Smallpath <smallpath2013@gmail.com>
2016-09-16 16:06:29 +0800

    [前端] 完成sidebar

---
+++ client_side/vue/src/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav id=sidebar class=behavior_1>
+        <div class=wrap>
+            <div class=profile>
+                <a href="/"> <img v-bind:src="logoPath" alt="Smallpath的小站"> </a> <span>Smallpath的小站</span></div>
+            <ul class=buttons>
+                <li>
+                    <a href="/" title="首页"> <i class="iconfont icon-home"></i> <span>首页</span></a>
+                    <li>
+                        <a href="/archives/" title="归档"> <i class="iconfont icon-archive"></i> <span>归档</span></a>
+                        <li>
+                            <a href="/tags/" title="标签"> <i class="iconfont icon-tags"></i> <span>标签</span></a>
+                            <li>
+                                <a v-link="{name:'about'}" title="关于"> <i class="iconfont icon-user"></i> <span>关于</span></a>
+                                <li>
+                                    <a href="/links/" title="友情链接"> <i class="iconfont icon-link"></i> <span>友链</span></a>
+            </ul>
+            <ul class=buttons>
+                <li>
+                    <a class=inline target=_blank href="https://github.com/Smallpath"> <i class="iconfont icon-github-v" title=GitHub></i></a>
+                    <a class=inline href="/rss.html"> <i class="iconfont icon-rss-v" title=RSS></i></a>
+                    <a class=inline href="/search.html"> <i class="iconfont icon-search" title=Search></i></a>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data () {
+    return {
+      siteName: 'Smallpath的小站',  
+      prefix: 'https://smallpath.me',
+      logoPath: "https://smallpath.me/static/upload/201605/logo.png",
+    }
+  }
+}
+</script>
+
+<style scoped>
+    @import '../assets/res/css/icon.css';
+    @import '../assets/res/css/sidebar.css';
+</style>
--- client_side/vue/src/main.js
+++ client_side/vue/src/main.js
@@ -1,8 +1,37 @@
+/* eslint-disable */
 import Vue from 'vue'
-import App from './App'
+import VueRouter from "vue-router";
+import VueResource from 'vue-resource'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+import App from './App'
+import Sidebar from './components/Sidebar'
+
+Vue.component('sidebar',Sidebar);
+
+let router = new VueRouter()
+
+router.map({
+    '/about': {
+        name:'about',
+        component: App
+    },
+    '/': {
+        name:'sidebar',
+        component: App
+    },
 })
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+
+router.redirect({
+  '*': '/'
+})
+
+router.start(Vue.extend({}), '#app')
+
