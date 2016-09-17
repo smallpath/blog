09dfe115dffee7f5aed6dcaf5c8ac484236db7ec
Smallpath <smallpath2013@gmail.com>
2016-09-17 18:39:39 +0800

    [前端] 完成Footer

--- client_side/vue/src/components/Footer.vue
+++ client_side/vue/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer id=footer class=inner> &copy; 2016&nbsp;-&nbsp; Smallpath的小站 &nbsp;-&nbsp;<a href="/">smallpath.me</a> <br> Powered by&nbsp;<a target=_blank
-            href="https://thinkjs.org">Vue</a>&nbsp;&amp;&nbsp;<a target=_blank rel=nofollow class=external href="https://github.com/75team/firekylin">Koa2</a>        
+    <footer id="footer" class="inner"> &copy; 2016&nbsp;-&nbsp; Smallpath的小站 &nbsp;-&nbsp;<a href="/">smallpath.me</a> <br> Powered by&nbsp;<a target=_blank
+            href="https://github.com/vuejs/vue">Vue</a>&nbsp;&amp;&nbsp;<a target=_blank rel=nofollow class=external href="https://github.com/koajs/koa/tree/v2.x">Koa2</a>        
     </footer>
 </template>
