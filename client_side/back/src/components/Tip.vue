bcc859f2e33d80ee4eabc278beac055f4f8e9751
Smallpath <smallpath2013@gmail.com>
2016-09-18 23:33:40 +0800

    [前端] 添加bootstrap

---
+++ client_side/back/src/components/Tip.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-if="shouldTipShow == true" class="fk-alert alert alert-' + type">{{text}}</div>
+    <div v-else></div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  props:{
+    shouldTipShow: Boolean,
+    type: String,
+    text: String,
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limi t CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
--- client_side/back/src/main.js
+++ client_side/back/src/main.js
@@ -1,8 +1,29 @@
+/* eslint-disable */
 import Vue from 'vue'
+import VueRouter from "vue-router";
+import VueResource from 'vue-resource'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+let router = new VueRouter()
+
+router.map({
+    '/': {
+        name:'main', 
+        component: App
+    },
 })
+
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+
+router.redirect({
+  '*': '/'
+})
+
+router.start(Vue.extend({}), 'body')
