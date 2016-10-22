34b1255bdb207a6afa99b5563ad0310a8428e04a
Smallpath <smallpath2013@gmail.com>
2016-10-22 22:48:23 +0800

    [前端-前台] 修复page查询的字符串类型错误

--- client/front/src/components/Pagination.vue
+++ client/front/src/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="pagination">
-        <router-link v-if="page > 1" :to="{ query: { page: page-1} }" class="prev">&laquo; 上一页</router-link>
-        <router-link v-if="page < totalPage" :to="{ query: { page: page+1}  }" class="next">下一页 &raquo;</router-link>
+        <router-link v-if="page > 1" :to="{ query: { page: parseInt(page) - 1} }" class="prev">&laquo; 上一页</router-link>
+        <router-link v-if="page < totalPage" :to="{ query: { page: parseInt(page) + 1}  }" class="next">下一页 &raquo;</router-link>
         <div class=center>
             <router-link :to="{name:'archive'}">博客归档</router-link>
         </div>
