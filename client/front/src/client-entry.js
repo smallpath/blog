55ea042f58a66df2befb3c9087bb6122ac5f195b
smallpath <smallpath2013@gmail.com>
2016-10-29 21:33:43 +0800

    [前端-前台] 准备谷歌统计

--- client/front/src/client-entry.js
+++ client/front/src/client-entry.js
@@ -2,10 +2,25 @@
 // require('es6-promise').polyfill()
 
 import './assets/res/js/base'
-import { app, store } from './main'
+import { app, router ,store } from './main'
 
+
+router.afterEach(function (route) {
+    console.log(route);
+	ga('send', {
+	  hitType: 'pageview',
+	  page: route.path,
+	  location: window.location.origin + route.path,
+	  title: route.name
+	});
+})
 
 store.replaceState(window.__INITIAL_STATE__)
 
 // actually mount to DOM
 app.$mount('#app')
+
+
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-86299315-1', 'auto');
+ga('send', 'pageview');
