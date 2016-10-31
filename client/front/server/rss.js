5d26138c7ab8a326826e777db6e6c5874d9556de
Smallpath <smallpath2013@gmail.com>
2016-10-31 22:56:01 +0800

    [前端-前台] 编码markdown内容; 完成rss

--- client/front/server/rss.js
+++ client/front/server/rss.js
@@ -9,21 +9,26 @@ let head = `<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
     <atom:link href="https://imququ.superfeedr.com/" rel="hub"/>
     <language>zh-cn</language>\r\n`
 
-let getUpdatedDate = date => `<lastBuildDate>${date}</lastBuildDate>`
+let getUpdatedDate = date => `    <lastBuildDate>${date}</lastBuildDate>\r\n`
 
 let tail = `  </channel>
 </rss>`
 
-let api = 'localhost:3000/api/post?conditions={"type":0}&select={"pathName":1,"updatedAt":1,"markdownContent":1}&sort=1&limit=10'
+let api = 'localhost:3000/api/post?conditions={"type":0}&select={"pathName":1,"updatedAt":1,"markdownContent":1,"title":1}&sort=1&limit=10'
 
 let getRssBodyFromBody = result => {
   let res = result.body
   let body = res.reduce((prev, curr) => {
     let date = new Date(curr.updatedAt).toUTCString()
+    let content = curr.markdownContent.replace(/&/g, '&amp;')
+                                      .replace(/</g, '&lt;')
+                                      .replace(/>/g, '&gt;')
+                                      .replace(/"/g, '&quot;')
+                                      .replace(/'/g, '&apos;')
     prev += `    <item>\r\n`
-    prev += `      <title>${curr.title}<title>\r\n`
+    prev += `      <title>${curr.title}</title>\r\n`
     prev += `      <link>${siteUrl}/post/${curr.pathName}</link>\r\n`
-    prev += `      <description>${curr.markdownContent}</description>\r\n`
+    prev += `      <description>${content}</description>\r\n`
     prev += `      <pubDate>${date}</pubDate>\r\n`
     prev += `      <guid>${siteUrl}/post/${curr.pathName}</guid>\r\n`
     prev += `    </item>\r\n`
