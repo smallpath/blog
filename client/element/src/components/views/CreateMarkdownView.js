5050c2fe25fad6b20f94f94ff9b4a110ef2658ce
smallpath <smallpath2013@gmail.com>
2016-11-22 23:19:53 +0800

    [前端-后台管理v2] 完成Markdown编辑器

---
+++ client/element/src/components/views/CreateMarkdownView.js
@@ -0,0 +1,17 @@
+import ItemList from '../containers/Markdown.vue'
+
+// This is a factory function for dynamically creating root-level list views,
+// since they share most of the logic except for the type of items to display.
+// They are essentially higher order components wrapping ItemList.vue.
+export default function (options) {
+  return {
+    name: `${options.name}-markdown-view`,
+    // this will be called during SSR to pre-fetch data into the store!
+    preFetch (store) {
+      return store.dispatch('FETCH_LIST', options)
+    },
+    render (h) {
+      return h(ItemList, { props: { options: options } })
+    }
+  }
+}
