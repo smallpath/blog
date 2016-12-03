ab8322a425e23873b622e6b6d8e62cff45cddc8b
smallpath <smallpath2013@gmail.com>
2016-12-03 23:54:07 +0800

    [前端-前台] 侧边栏图片适配移动端

--- client/front/src/components/Header.vue
+++ client/front/src/components/Header.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <div id="header">
+    <div id="header" :style="{ 
+        'background-image': option.sidebarImageUrl 
+        ? 'url(' + option.sidebarImageUrl + ')' : ''
+      }">
       <div class="btn-bar"><i></i></div>
-      <h1><a href="/">{{ siteInfo.title.value }}</a></h1>
-      <a class=me href="/about"><img :src="siteInfo.logoUrl.value" :alt="siteInfo.title.value"></a>
+      <h1><a :style="{'color': option.sidebarFontColor || ''}" href="/">{{ siteInfo.title.value }}</a></h1>
+      <a class=me href="/about">
+        <img :src="siteInfo.logoUrl.value" 
+          :alt="siteInfo.title.value">
+      </a>
     </div>
     <div id="sidebar-mask"></div>
   </div>
@@ -14,6 +20,11 @@ export default {
   data () {
     return {
       siteInfo: this.$store.getters.siteInfo
+    }
+  },
+  computed: {
+    option () {
+      return  this.$store.state.theme.option
     }
   },
   serverCacheKey: props => {
