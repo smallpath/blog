bd1f1902d15d0824131a623042edeb3c8b3f5b45
smallpath <smallpath2013@gmail.com>
2016-12-07 12:49:55 +0800

    [前端-前台] 启用谷歌搜索

--- client/front/src/server-entry.js
+++ client/front/src/server-entry.js
@@ -3,12 +3,11 @@ import { app, router, store } from './main'
 const isDev = process.env.NODE_ENV !== 'production'
 
 export default context => {
-  // set router's location
   router.push(context.url)
   let current = router.history.current
-  context.path = current.path,
-  context.query = current.query,
-  context.params = current.params,
+  context.path = current.path
+  context.query = current.query
+  context.params = current.params
   context.url = current.fullPath
 
   const s = isDev && Date.now()
