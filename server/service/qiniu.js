a8492f7682a9ed6176dd460da4120121db42cdf6
smallpath <smallpath2013@gmail.com>
2017-01-25 15:23:07 +0800

    [后端] 去除七牛日志输出

--- server/service/qiniu.js
+++ server/service/qiniu.js
@@ -14,7 +14,6 @@ let fops = 'imageMogr2/format/webp'
 
 const policy = (name, fileName) => {
     let encoded = new Buffer(`${qiniuBucketName}:webp/${fileName}`).toString('base64')
-    console.log(`${fops}|saveas/${encoded}`)
     let persist
     if (qiniuPipeline !== '') {
         persist = {
