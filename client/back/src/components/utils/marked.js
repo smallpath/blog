a31c7135f04116a3f043e172bee3fda20810677a
smallpath <smallpath2013@gmail.com>
2017-01-28 19:58:30 +0800

    [前端-后台管理] 启用高亮

--- client/back/src/components/utils/marked.js
+++ client/back/src/components/utils/marked.js
@@ -1,10 +1,10 @@
 import marked from 'marked'
-// import hljs from 'highlight.js'
+import hljs from 'highlight.js'
 
-// marked.setOptions({
-//   highlight: function (code) {
-//     return hljs.highlightAuto(code).value
-//   }
-// })
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
 
 export default marked
