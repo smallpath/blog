2b39b629b3b68d0fbddf9626dc7c519711fe5697
smallpath <smallpath2013@gmail.com>
2017-02-03 15:25:58 +0800

    [前端-后台管理] 修复无toc时无法提交的问题

--- client/back/src/components/containers/Post.vue
+++ client/back/src/components/containers/Post.vue
@@ -137,6 +137,7 @@ export default {
     validate () {
       this.form.summary = marked(this.form.markdownContent.split('<!--more-->')[0])
       this.form.content = marked(this.form.markdownContent.replace(/<!--more-->/g, ''))
+      this.form.markdownToc = this.form.markdownToc || ''
       this.form.toc = marked(this.form.markdownToc)
       if (this.form.createdAt === '') {
         this.form.createdAt = moment().format('YYYY-MM-DD HH:mm:ss')
