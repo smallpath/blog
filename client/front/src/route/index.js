253f29349bc776a0f80efd1580ec568efc6e4b37
smallpath <smallpath2013@gmail.com>
2017-02-04 00:52:55 +0800

    [前端-前台] 按需加载标签详情页

--- client/front/src/route/index.js
+++ client/front/src/route/index.js
@@ -8,26 +8,18 @@ Vue.use(VueMeta)
 import Footer from '../components/Footer'
 Vue.component('my-footer', Footer)
 
-// Can't make a array reduce or the number of chunks will increase twice,
-// because System.import is ES6 module, which requires a static module imports
-const BlogPager = process.BROWSER
-  ? () => System.import('../components/BlogPager')
-  : require('../components/BlogPager')
-const Archive = process.BROWSER
-  ? () => System.import('../components/Archive')
-  : require('../components/Archive')
-const Tag = process.BROWSER
-  ? () => System.import('../components/Tag')
-  : require('../components/Tag')
-const PostContainer = process.BROWSER
-  ? () => System.import('../components/PostContainer')
-  : require('../components/PostContainer')
+const BlogPager = require('../components/BlogPager')
+const Archive = require('../components/Archive')
+const Tag = require('../components/Tag')
+const PostContainer = require('../components/PostContainer')
+const PageContainer = require('../components/PageContainer')
+
+// System.import is of ES6 module, which is designed to be statically analyzable
+// It means that you can't write `let path = './main.js'; System.import(path)`
+// Must use it by `System.import('./main.js')`
 const TagPager = process.BROWSER
   ? () => System.import('../components/TagPager')
   : require('../components/TagPager')
-const PageContainer = process.BROWSER
-  ? () => System.import('../components/PageContainer')
-  : require('../components/PageContainer')
 
 export default new VueRouter({
   mode: 'history',
