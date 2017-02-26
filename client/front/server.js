6866c854e06a56c36ac9aa8c14e71c65021b8a0b
smallpath <smallpath2013@gmail.com>
2017-02-26 13:23:10 +0800

    [前端-前台] 优化service worker

--- client/front/server.js
+++ client/front/server.js
@@ -8,7 +8,6 @@ const express = require('express')
 const schedule = require('node-schedule')
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
 const request = require('axios')
-const uuid = require('uuid')
 
 const sendGoogleAnalytic = require('./middleware/serverGoogleAnalytic')
 const getRobotsFromConfig = require('./server/robots.js')
@@ -17,8 +16,6 @@ const { api: rssApi, getRssBodyFromBody } = require('./server/rss.js')
 const inline = isProd ? fs.readFileSync(resolve('./dist/styles.css'), 'utf-8') : ''
 const config = require('./server/config')
 const favicon = require('./server/favicon')
-const titleReg = /<.*?>(.+?)<.*?>/
-const expires = 3600 * 1000 * 24 * 365 * 2
 
 const chunkObj = {}
 let appHash = ''
@@ -141,23 +138,8 @@ config.flushOption().then(() => {
       const { title, link, meta } = context.meta.inject()
       const titleText = title.text()
       const metaData = `${titleText}${meta.text()}${link.text()}`
-      const matched = titleText.match(titleReg)
-      let clientId = req.cookies.id
-      if (!clientId) {
-        clientId = uuid.v4()
-        res.cookie('id', clientId, {
-          expires: new Date(Date.now() + expires)
-        })
-      }
       const chunk = html.head.replace('<title></title>', metaData)
       res.write(chunk)
-      sendGoogleAnalytic(req, res, next, {
-        dt: matched ? matched[1] : config.title,
-        dr: req.url,
-        dp: req.url,
-        z: +Date.now(),
-        cid: clientId
-      })
     })
 
     renderStream.on('data', chunk => {
