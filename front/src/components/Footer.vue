ee65287e29aee9e5ee63bc4b229d175b398e7339
smallpath <smallpath2013@gmail.com>
2017-04-19 17:01:40 +0800

    [前端] 页脚显示备案号

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
   <footer id="footer" class="inner">
     &copy; 2016&nbsp;-&nbsp; {{ siteInfo.title.value }} &nbsp;-&nbsp;
-    <a target=_blank href="https://github.com/smallpath/blog">博客源码</a>
+    <a v-if="!(siteInfo.miitbeian.value)" target=_blank href="https://github.com/smallpath/blog">博客源码</a>
+    <a v-else target="_blank" rel="nofollow noopener" 
+        href="http://www.miitbeian.gov.cn/"
+        style="color: #666"
+    >{{ siteInfo.miitbeian.value }}</a>
     <br> Powered by&nbsp;
     <a target=_blank href="https://github.com/vuejs/vue">Vue2</a>
     &nbsp;&amp;&nbsp;
