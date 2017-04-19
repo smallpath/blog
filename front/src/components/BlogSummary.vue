9b581a4ee02babaed46553b2e5ecaa5a1f4f3116
smallpath <smallpath2013@gmail.com>
2017-04-19 16:54:27 +0800

    [前端] 增加阅读更多链接

--- front/src/components/BlogSummary.vue
+++ front/src/components/BlogSummary.vue
@@ -4,7 +4,10 @@
       <div class="date">{{ article.createdAt }}</div>
     </div>
     <h1 class="title"> <router-link :to="{ name:'post', params:{ pathName: article.pathName } }" >{{ article.title }}</router-link></h1>
-    <div class="entry-content" v-html="filterWebp(article.summary)"></div>
+    <div class="entry-content">
+      <div v-html="filterWebp(article.summary)"/>
+      <router-link :to="{ name:'post', params:{ pathName: article.pathName } }" >阅读更多 »</router-link>
+    </div>
   </article>
 </template>
 
