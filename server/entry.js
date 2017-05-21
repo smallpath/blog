607b17049673adac5ee3bcded37e5e1c19f22b09
smallpath <smallpath2013@gmail.com>
2017-05-21 12:24:53 +0800

    [后端] 解决koa-ratelimit未被转码导致node v6语法分析错误无法运行的问题 (Fixed #17)

--- server/entry.js
+++ server/entry.js
@@ -1,5 +1,10 @@
 require('babel-register')({
-  plugins: ['transform-async-to-generator']
+  plugins: ['transform-async-to-generator'],
+  ignore: function(filename) {
+    if (filename.includes('koa-ratelimit')) return false
+    if (filename.includes('node_modules')) return true
+    return false
+  }
 })
 
 require('./app.js')
