e553a8e6aab9e7a5a440af0e862d98c1db716b9a
smallpath <smallpath2013@gmail.com>
2017-06-14 14:30:14 +0800

    [后端] 修复logTime插件重复导出的问题

--- server/plugins/beforeUseRoutes/logTime.js
+++ server/plugins/beforeUseRoutes/logTime.js
@@ -1,6 +1,6 @@
 const log = require('../../utils/log')
 
-module.exports = module.exports = class {
+module.exports = class {
   async beforeUseRoutes({ app, redis }) {
     app.use(async (ctx, next) => {
       const start = new Date()
